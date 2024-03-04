b0efab6a3152eb8ba7adc39e0dbcee99f1478d3e
yizhen05 <usukumonez@gmail.com>
2024-03-04 22:26:42 +0900

    [fix]速度の計算でTime.deltaTimeを用いるようにした

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour
 {
+    static public float speed = 20.0f;
     void Update()
     {
-        transform.Translate(0, 0.2f, 0);
+        // transform.Translate(0, 0.2f, 0);
+        transform.Translate(0, 0.2f * speed * Time.deltaTime, 0);
 
         if (transform.position.y > 5)
         {
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,26 +16,29 @@ public class RocketController : MonoBehaviour
 
     void Update()
     {
-        // ユーザーからの入力を受け付けてロケットを移動させる.-2<x<2
         if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -2)
         {
-            transform.Translate(-0.1f * speed, 0, 0);
+            //transform.Translate(-0.1f * speed, 0, 0);
+            transform.Translate(-0.1f * speed * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 2)
         {
-            transform.Translate(0.1f * speed, 0, 0);
+            //transform.Translate(0.1f * speed, 0, 0);
+            transform.Translate(0.1f * speed * Time.deltaTime, 0, 0);
         }
-        //タッチ操作
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             if ((touch.position.x < Screen.width / 2) && transform.position.x > -2)
             {
-                transform.Translate(-0.1f * speed, 0, 0);
+                // transform.Translate(-0.1f * speed, 0, 0);
+                transform.Translate(-0.1f * speed * Time.deltaTime, 0, 0);
             }
             else if ((touch.position.x > Screen.width / 2) && transform.position.x < 2)
             {
-                transform.Translate(0.1f * speed, 0, 0);
+                // transform.Translate(0.1f * speed, 0, 0);
+                transform.Translate(0.1f * speed * Time.deltaTime, 0, 0);
             }
         }
 
