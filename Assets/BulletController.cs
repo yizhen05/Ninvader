7fb35d1951f693e408fbfb7003791abc35a4b690
yizhen05 <usukumonez@gmail.com>
2024-06-22 01:57:34 +0900

    なにいじったっけ

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour
 {
+    public GameObject explosionPrefab;
     static public float speed = 20.0f;
+
     void Update()
     {
         // transform.Translate(0, 0.2f, 0);
@@ -17,6 +19,7 @@ public class BulletController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D coll)
     {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         Destroy(coll.gameObject);
         Destroy(gameObject);
     }
